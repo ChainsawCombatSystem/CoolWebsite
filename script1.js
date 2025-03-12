f4f54ce61f2802c50809ac9e39925a8b5a08886e
ChainsawCombatSystem <goldvader300@gmail.com>
2025-03-12 21:52:00 +0100

    e

--- script1.js
+++ script1.js
@@ -1,1 +1,19 @@
-// JavaScript Document
+	var pages=0;
+	
+	function changeNow(){
+		document.getElementById("box").style.backgroundImage="url('slend.jpeg')";
+	}
+	function collectPage(){
+		pages++;
+		updatePages();
+	}
+	function updatePages(){
+	document.getElementById("collectedPages").innerHTML="You have "+pages+" pages!!!";
+	if(pages>=8){
+		document.getElementById("box").style.backgroundImage="url('slender.jpg')";
+	}
+	else if(pages<8){
+		document.getElementById("box").style.backgroundImage="url('slend.jpeg')";
+	}
+	}
+updatePages();
